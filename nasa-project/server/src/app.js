c66558e44586bd3cb35dceccf8c9c9e714439ca9
Adrián Escat <adrianescat@Adrians-MacBook-Pro.local>
2022-04-06 21:31:32 -0300

    Serve optimized FE app from the server

--- nasa-project/server/src/app.js
+++ nasa-project/server/src/app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 const app = express();
+
 const planetsRouter = require('./routes/planets/planets.router');
 
 // Middlewares
@@ -8,6 +10,11 @@ app.use(cors({
   origin: 'http://localhost:3000',
 }));
 app.use(express.json());
+app.use(express.static(path.join(__dirname, '..', 'public')))
 app.use(planetsRouter);
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
+});
 
 module.exports = app;
