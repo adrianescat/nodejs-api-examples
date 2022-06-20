f23b2d125c5d1749e56ac85e5589efd236a5b1d4
Adrián Escat <adrianescat@Adrians-MacBook-Pro.local>
2022-06-20 16:38:21 -0300

    Testing APIs with Jest

---
+++ nasa-project/server/src/routes/launches/launches.test.js
@@ -0,0 +1,21 @@
+describe('Test GET /launches', () => {
+  test('it should respond with 200 success', () => {
+    const response = 200;
+
+    expect(response).toBe(200);
+  })
+})
+
+describe('Test POST /launches', () => {
+  test('it should respond with 200 success', () => {
+
+  })
+
+  test('it should catch missing required properties', () => {
+
+  })
+
+  test('it should catch invalid dates', () => {
+
+  })
+})
