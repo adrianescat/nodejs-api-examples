f6fef9953be4fc77a11af78286be762179416ddb
Adrián Escat <adrianescat@Adrians-MacBook-Pro.local>
2022-06-20 16:59:58 -0300

    Testing HTTP API Endpoints with supertest package (POST)

--- nasa-project/server/src/routes/launches/launches.test.js
+++ nasa-project/server/src/routes/launches/launches.test.js
@@ -11,8 +11,34 @@ describe('Test GET /launches', () => {
 })
 
 describe('Test POST /launches', () => {
-  test('it should respond with 200 success', () => {
+  // We create an object to reuse the data in multiple tests and scenarios
+  const completeLaunchData = {
+    mission: 'USS Enterprise',
+    rocket: 'NCC 1701-D',
+    target: 'Kepler-186 f',
+    launchDate: 'January 4, 2028',
+  }
 
+  const launchDataWithoutDate = {
+    mission: 'USS Enterprise',
+    rocket: 'NCC 1701-D',
+    target: 'Kepler-186 f',
+  }
+
+  test('it should respond with 201 success', async () => {
+    const response = await request(app)
+      .post('/launches')
+      .send(completeLaunchData)
+      .expect('Content-Type', /json/)
+      .expect(201);
+
+    const requestDate = new Date(completeLaunchData.launchDate).valueOf();
+    const responseDate = new Date(response.body.launchDate).valueOf();
+
+    // We need to match dates separately because the server will respond a different date string
+    expect(requestDate).toBe(responseDate);
+
+    expect(response.body).toMatchObject(launchDataWithoutDate)
   })
 
   test('it should catch missing required properties', () => {
