fe371aaf353b32dd9c4d6f86d47bb45c1970b9e1
Adrián Escat <adrianescat@Adrians-MacBook-Pro.local>
2023-01-12 17:54:53 -0300

    using cluster module

--- performace/src/server.js
+++ performace/src/server.js
@@ -1,24 +1,33 @@
 const express = require('express');
+const cluster = require('cluster');
 
 const app = express();
 
 function delay(duration) {
   const startTime = Date.now();
   while (Date.now() - startTime < duration) {
-    // event loop completly blocked. Try yo open two tabs at the same
-    // time and make the /timer request. The second call will run more than
-    // 9 seconds because the first one was blocking the event loop
+    // Now, you can try again with 2 tabs in the browser. Remember to disable cache
+    // in the network tab. You will see that each request take only 9 seconds, and the 
+    // ids in the responses are different
   }
 }
 
 app.get('/', (req, res) => {
-  res.send('Performance example')
+  res.send(`Performance example: ${process.pid}`)
 })
 
 app.get('/timer', (req, res) => {
   // delay the response
   delay(9000); // 9 seconds
-  res.send('Ding Ding Ding')
+  res.send(`Ding Ding Ding: ${process.pid}`)
 })
 
-app.listen(3000);
+if (cluster.isMaster) {
+  console.log('Master has been started');
+  cluster.fork(); // each worker runs the same server.js code
+  cluster.fork();
+} else {
+  console.log('Worker process started');
+  // Node will know that workers were created and are ready to listen
+  app.listen(3000);
+}
